7f48ea143760fff54e2faf55d645a953217946f7
Jeffrey O. Adams <frexjeff@gmail.com>
2023-01-10 19:01:51 +0000

    changed java version from 17 to 11

--- src/main/java/io/jenkins/pipeline/api/DemoResource.java
+++ src/main/java/io/jenkins/pipeline/api/DemoResource.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/demo")
 public class DemoResource {
-
     public void start(){
         System.out.println("Demo App with Jenkins Pipeline with Docker");
     }
